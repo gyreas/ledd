050657f129d03d7c659bea6f01f950d974bdd61b
Saheed Adeleye <aadesaed@gmail.com>
2023-09-28 21:57:15 +0100

    something is working -- so far

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use crate::buffer::{Buffer, Line};
+use ledd::*;
 
 fn main() {
-    let b = Buffer::new();
-    println!("Hello, world!");
+    let text =
+        String::from("I am become Death, destroyer of worlds.\nHitherto unknown function.\n");
+    let fname = "buffer.test".to_owned();
+    let b = Buffer::init(fname, text.clone());
+    b.insert_line_with_text(text);
+    let emb = Buffer::empty();
+    dbg!(b);
+    dbg!(emb);
 }
