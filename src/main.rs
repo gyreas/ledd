94bbc95b9153e9464e438f5e889ffe7c5071db09
Saheed Adeleye <aadesaed@gmail.com>
2023-10-02 00:01:11 +0100

    main.rs is still a kind of testing area for now

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use ledd::*;
 
 fn main() {
     let text =
-        String::from("I am become Death, destroyer of worlds.\nHitherto unknown function.\n");
-    let fname = "buffer.test".to_owned();
+        String::from("I am become Death\n, destroyer \nof worlds.\nHitherto \nunknown function.\n");
+    let fname = "buffer.test";
     let b = Buffer::init(fname, text.clone());
-    b.insert_line_with_text(text);
+    // b.insert_line_with_text(text);
     let emb = Buffer::empty();
-    dbg!(b);
-    dbg!(emb);
+    println!("{:?}", b);
+    println!("{:?}", emb);
 }
