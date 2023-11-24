348963e5d8c34e553e8260484f6daebaeeccecfc
Saheed Adeleye <aadesaed@gmail.com>
2023-11-24 22:01:11 +0100

    Something is working: implemented (very) basic input line and editing

--- src/tiny.rs
+++ src/tiny.rs
@@ -1,24 +1,20 @@
 use crossterm::{
-    cursor::{self, *},
+    cursor::*,
     event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers},
-    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType, ScrollDown, ScrollUp},
+    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
     ExecutableCommand, QueueableCommand,
 };
 
-use std::fmt::Write as _;
-use std::io::{stdout, Cursor as CursoredBuffer, Write as _};
-
-struct Lino {
-    lineno: u16,
-    cursor_col: u16,
-}
+use std::io::{stdout, Write as _};
 
 pub fn tiny_input() -> crossterm::Result<()> {
-    enable_raw_mode()?;
-    let (ncols, nrows) = size()?;
     let mut stdout = stdout();
-    let mut term_buf = CursoredBuffer::new(Vec::new());
-    let mut cursor_pos = term_buf.position();
+
+    let mut prompt_buf = String::new();
+    let mut count_enters = 0;
+
+    enable_raw_mode()?;
+    stdout.execute(Clear(ClearType::FromCursorUp))?;
 
     loop {
         if let Event::Key(keyev) = event::read()? {
@@ -30,7 +26,7 @@ pub fn tiny_input() -> crossterm::Result<()> {
                     state: KeyEventState::NONE,
                 } => {
                     write!(&mut stdout, "Wrote: ")?;
-                    stdout.write_all(term_buf.get_mut())?;
+                    stdout.write_all(prompt_buf.as_bytes())?;
                     writeln!(&mut stdout)?;
                     stdout
                         .queue(MoveToNextLine(1))?
@@ -39,49 +35,62 @@ pub fn tiny_input() -> crossterm::Result<()> {
                     disable_raw_mode()?;
                     std::process::exit(1)
                 }
+
+                KeyEvent {
+                    code: KeyCode::Char('l'),
+                    modifiers: KeyModifiers::CONTROL,
+                    kind: KeyEventKind::Press,
+                    state: KeyEventState::NONE,
+                } => {
+                    stdout.queue(Clear(ClearType::FromCursorUp))?;
+                    count_enters = 0;
+                }
                 KeyEvent {
                     code: KeyCode::Enter,
                     modifiers: KeyModifiers::NONE,
                     kind: KeyEventKind::Press,
                     state: KeyEventState::NONE,
                 } => {
-                    cursor_pos += 1;
-                    term_buf.set_position(cursor_pos);
+                    stdout.queue(SavePosition)?;
+                    if prompt_buf.is_empty() {
+                        stdout.queue(MoveTo(0, count_enters))?;
+                        writeln!(&mut stdout, "-")?;
+                    } else {
+                        // for line in &lines {
+                        //     stdout.queue(MoveTo(0, enters))?;
+                        //     writeln!(&mut stdout, "{}", line)?;
+                        // }
+                        // }
+                        // lines.clear();
+                        // }
+                        // KeyEvent {
+                        //     code: KeyCode::Enter,
+                        //     modifiers: KeyModifiers::NONE,
+                        //     kind: KeyEventKind::Press,
+                        //     state: KeyEventState::NONE,
+                        // } => {
+                        let edge = size()?;
+                        stdout.queue(MoveTo(0, count_enters))?;
+                        writeln!(&mut stdout, "{}", prompt_buf)?;
+                        count_enters += 1;
+                        // lines.push(term_buf.clone());
+                        prompt_buf.clear();
+                        stdout.queue(RestorePosition)?;
 
-                    writeln!(&mut term_buf, "Newline")?;
-                    writeln!(&mut stdout, "Newline")?;
-                    stdout.queue(MoveToNextLine(1))?;
+                        stdout
+                            .queue(Clear(ClearType::CurrentLine))?
+                            .queue(MoveTo(0, edge.1))?;
+                    }
                 }
                 key => match key.code {
-                    KeyCode::Char('j') => {
-                        write!(&mut stdout, "size: {}", nrows - 1)?;
-                        write!(&mut stdout, "jp: {cursor_pos}")?;
-                        if cursor_pos + 1 < nrows as u64 && !term_buf.get_ref().is_empty() {
-                            cursor_pos += 1;
-                            term_buf.set_position(cursor_pos);
-                            write!(&mut stdout, "jc: {cursor_pos}")?;
-                            stdout.queue(MoveToNextLine(1))?;
-                            //     stdout.queue(ScrollUp(1))?;
-                        }
-                    }
-                    KeyCode::Char('k') => {
-                        write!(&mut stdout, "kc: {cursor_pos}")?;
-                        if cursor_pos >= 1 {
-                            cursor_pos -= if cursor_pos >= 1 { 1 } else { 0 };
-                            term_buf.set_position(cursor_pos);
-
-                            // stdout.queue(ScrollDown(1))?;
-                            stdout.queue(MoveToPreviousLine(1))?;
-                        }
-                    }
-                    KeyCode::Char('h') => {
-                        stdout.queue(MoveLeft(1))?;
-                    }
-                    KeyCode::Char('l') => {
-                        stdout.queue(MoveRight(1))?;
-                    }
                     KeyCode::Backspace => {
+                        let _ = prompt_buf.pop();
+                        let _ = stdout.queue(MoveLeft(1))?.write(" ".as_bytes())?;
                         stdout.queue(MoveLeft(1))?;
+                    }
+                    KeyCode::Char(ch) if ch.is_alphanumeric() || ch.is_whitespace() => {
+                        prompt_buf.push(ch);
+                        stdout.write_all(ch.to_string().as_bytes())?;
                     }
                     _ => (),
                 },
